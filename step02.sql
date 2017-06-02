6f1b26697990182e3cf769ad1d9cbfafb87500ee
Brian Stout <brianjonstout@gmail.com>
2017-06-02 13:34:17 -0400

    Step02, add triggers to check for requirements.

---
+++ step02.sql
@@ -0,0 +1,40 @@
+drop table if exists players;
+drop trigger if exists InsertJerseyCheck;
+drop trigger if exists UpdateJerseyCheck;
+
+CREATE TABLE players (
+				id INTEGER(4) not null primary key, 
+				firstname VARCHAR(10) not null,
+				lastname VARCHAR(10) not null,
+				team VARCHAR(10),
+				playernum INTEGER(4)
+			);
+
+
+DELIMITER //
+CREATE TRIGGER InsertJerseyCheck BEFORE INSERT ON players
+FOR EACH ROW BEGIN
+  IF (NEW.team IS NULL AND NEW.playernum IS NOT NULL) THEN
+    SIGNAL SQLSTATE '45000'
+    SET MESSAGE_TEXT = 'If player has a number he must have a team';
+  END IF;
+  IF (NEW.team IS NOT NULL and NEW.playernum IS NULL) THEN
+	SIGNAL SQLSTATE '45000'
+	SET MESSAGE_TEXT = 'If player has a team he must have a team number';
+  END IF;
+END//
+CREATE TRIGGER UpdateJerseyCheck BEFORE UPDATE ON players
+FOR EACH ROW BEGIN
+  IF (NEW.team IS NULL AND NEW.playernum IS NOT NULL) THEN
+    SIGNAL SQLSTATE '45000'
+    SET MESSAGE_TEXT = 'If player has a number he must have a team';
+  END IF;
+  IF (NEW.team IS NOT NULL and NEW.playernum IS NULL) THEN
+	SIGNAL SQLSTATE '45000'
+	SET MESSAGE_TEXT = 'If player has a team he must have a team number';
+  END IF;
+END//
+DELIMITER ;
+
+
+
